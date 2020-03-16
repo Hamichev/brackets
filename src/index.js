df556a170d13db833814da22a02e7e83e911f0f2
Hamichev <60568270+Hamichev@users.noreply.github.com>
2020-03-16 20:23:00 +0300

    init: create code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr=str.split(''),N=0;
+   for (let i=0;i<arr.length;i++)
+   {
+       if (arr[i]==")")
+       {
+           for (let j=i-1;j>=0;j--)
+           {
+               if (arr[j]=="(" && N!=1) {arr[j]="*";arr[i]="*";break;} else if (arr[j]=="[" || arr[j]=="{" || arr[j]=="]" || arr[j]=="}") {document.write("Fail");return false;} else if (arr[j]=="|") {N++;} else if (j==0) {document.write("Fail");return false;}
+           }
+       }
+       N=0;
+       if (arr[i]=="]")
+       {
+           for (let j=i-1;j>=0;j--)
+           {
+               if (arr[j]=="[" && N!=1) {arr[j]="*";arr[i]="*";break;} else if (arr[j]=="(" || arr[j]=="{" || arr[j]==")" || arr[j]=="}") {document.write("Fail");return false;} else if (arr[j]=="|") {N++;} else if (j==0) {document.write("Fail");return false;}
+           }
+       }
+       N=0;
+       if (arr[i]=="}")
+       {
+           for (let j=i-1;j>=0;j--)
+           {
+               if (arr[j]=="{" && N!=1) {arr[j]="*";arr[i]="*";break;} else if (arr[j]=="[" || arr[j]=="(" || arr[j]=="]" || arr[j]==")") {document.write("Fail");return false;} else if (arr[j]=="|") {N++;} else if (j==0) {document.write("Fail");return false;}
+           }
+       }
+       N=0;
+   }
+
+   for (let i=0;i<arr.length;i++)
+   {
+    if (arr[i]=="|")
+    {
+        for (let j=i+1;j<=arr.length;j++)
+        {
+            if (arr[j]=="|") {arr[j]="*";arr[i]="*";break;} else if (arr[j]=="[" || arr[j]=="(" || arr[j]=="]" || arr[j]==")" || arr[j]=="{" || arr[j]=="}") {document.write("Fail");return false;} else if (j==arr.length) {document.write("Fail");return false;}
+        }
+    }
+   }
+   for (let i=0;i<arr.length;i++)
+   {
+       if (arr[i]!="*") {document.write("Fail");return false;}
+   }
+   document.write("Arbeiten!");
+   return true;
 }
